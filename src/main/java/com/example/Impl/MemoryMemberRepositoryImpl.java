471f1045af418c5add056a224bc97487e8cb8aca
ued123 <32861341+ued123@users.noreply.github.com>
2022-05-03 22:46:15 +0900

    Inflean 4 (#9)
    * # 인프런 김영한 spring 기초 공부
    싱글톤 Configuration의 마법
    - AppConfig 빈이 생성될때
    - 생성자로만 봤을때는 MemberServiceImpl은 3번 호출된다.
    - 하지만 실제 ApplicationContext가 해당 AppConfig를 초기화 할떄는 1번만 호출된다.
    - 이유는 다음시간에
    
    * # 인프런 김영한 spring 기초 공부
    @ComponentScan 원리
    - 말그대로 @Component 를 빈으로 등록해주는 어노테이션
    - @Configuration도 안에 @Component가 있기떄문에 빈으로 등록된다.
    
    @Autowired 원리
    - 어노테이션이 붙은 역할(인터페이스)등에 구현체 빈을 자동으로 찾아서 주입해준다.
    - 구현체 탐색시 기본전략은 해당 역할에 해당하는 타입의 구현체 빈 찾아서 주입한다.

--- src/main/java/com/example/Impl/MemoryMemberRepositoryImpl.java
+++ src/main/java/com/example/Impl/MemoryMemberRepositoryImpl.java
@@ -2,10 +2,12 @@ package com.example.Impl;
 
 import com.example.dto.Member;
 import com.example.role.MemberRepository;
+import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class MemoryMemberRepositoryImpl implements MemberRepository {
 
     private Map<Long,Member> userStore = new HashMap();
--- src/main/java/com/example/Impl/PercentDiscountPolicy.java
+++ src/main/java/com/example/Impl/PercentDiscountPolicy.java
@@ -3,7 +3,9 @@ package com.example.Impl;
 import com.example.dto.Member;
 import com.example.meta.Grade;
 import com.example.role.DiscountPolicy;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PercentDiscountPolicy implements DiscountPolicy {
 
     private int percentDiscount = 10;
