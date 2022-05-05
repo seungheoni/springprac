6866836a9be91c292b4ea6fa9aaccfcf7a4930c6
ued123 <32861341+ued123@users.noreply.github.com>
2022-05-05 15:30:14 +0900

    ComponentScan 충돌 케이스 공부 (#10)
    * # 인프런 김영한 spring 기초 공부
    싱글톤 Configuration의 마법
    - AppConfig 빈이 생성될때
    - 생성자로만 봤을때는 MemberServiceImpl은 3번 호출된다.
    - 하지만 실제 ApplicationContext가 해당 AppConfig를 초기화 할떄는 1번만 호출된다.
    - 이유는 다음시간에
    
    * # 인프런 김영한 spring 기초 공부
    @ComponentScan 원리
    - 말그대로 @Component 를 빈으로 등록해주는 어노테이션
    - @Configuration도 안에 @Component가 있기떄문에 빈으로 등록된다.
    
    @Autowired 원리
    - 어노테이션이 붙은 역할(인터페이스)등에 구현체 빈을 자동으로 찾아서 주입해준다.
    - 구현체 탐색시 기본전략은 해당 역할에 해당하는 타입의 구현체 빈 찾아서 주입한다.
    
    * # 인프런 김영한 spring 기초 공부
    ComponentScan시 충돌 케이스
    - 빈 등록시 기본 전략은 클래스 이름으로 등록되는데. 클래스 이름이 중복되었을때 ConflictingBeanDefinitionException 예외 발생
    
    - 자동스캔 vs 수동 빈 등록시 수동 빈등록이 우선순위가 높지만 최근 부트에서는 해당 부분에서도 예외를 던지도록 만들어졌다. (대부분 의도하지 않았기 떄문)

---
+++ src/test/java/com/example/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,49 @@
+package com.example.scan.filter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConfigTest {
+
+
+    @Test
+    void filterScan() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ComponentFilterConfig.class);
+
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        BeanB beanB = ac.getBean("beanB", BeanB.class);
+
+        assertNotNull(beanA);
+        assertNull(beanB);
+    }
+
+
+    /*
+        Component Filter를 통해 스캔시 제외하고 포함하는 정책을 설정 할 수 있다.
+        FilterType은 5가지 옵션이 있다.
+        - ANNOTATION: 기본값, 애노테이션을 인식해서 동작한다. ex) org.example.SomeAnnotation
+        - ASSIGNABLE_TYPE: 지정한 타입과 자식 타입을 인식해서 동작한다. ex) org.example.SomeClass
+        - ASPECTJ: AspectJ 패턴 사용
+          ex) org.example..*Service+
+        - REGEX: 정규 표현식
+          ex) org\.example\.Default.*
+        - CUSTOM: TypeFilter 이라는 인터페이스를 구현해서 처리 ex) org.example.MyTypeFilter
+     */
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION , classes = MyIncludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION , classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterConfig {
+
+    }
+
+}
